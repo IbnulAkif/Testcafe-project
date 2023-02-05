6dcd4a3f4401d441bba8d51cadf31f7f19f913ea
IbnulAkif <ibnulakifcse@gmail.com>
2023-02-05 23:35:09 +0600

    Signup Code

--- login.test.js
+++ login.test.js
@@ -1,9 +1,18 @@
 import { Selector } from 'testcafe'
 
 fixture `Getting Started`
-.page `http://zero.webappsecurity.com/`
+.page `https://www.demoblaze.com/`
 
  
 test('Valid Login test', async t => {
+    await t.click("#signin2");
+    await t.typeText("#sign-username","Ibnul16");
+    await t.typeText("#sign-password","Ibnul16");
+    //await t.click(Selector("button").withText("Sign up"));
+    await t
+    .setNativeDialogHandler(() => true)
+    .click(Selector("button").withText("Sign up"));
+    //await t.debug();
+    
        
     });
